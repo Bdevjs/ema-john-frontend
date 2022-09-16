c8b59bdf95949bd3f4b32dcf3c358cefd8534a43
mehadihassansovon <91381812+mehadihassansovon@users.noreply.github.com>
2022-09-17 01:03:00 +0600

    pagination done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import useProducts from '../../hooks/useProducts';
+//import useProducts from '../../hooks/useProducts';
 import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const [products, setProducts] = useProducts();
+    //const [products, setProducts] = useProducts(); 
+    //this section useFor product hooks
+    // get product from another file
     const [cart, setCart] = useState([]);
+    const [pages, setPages] = useState(0);
+    const [selectpage, setSelectPage] = useState(0);
+    const [size, setSize] = useState(10);
+    const [products,setProducts] = useState([]);
 
     useEffect( () =>{
         const storedCart = getStoredCart();
@@ -23,6 +29,26 @@ const Shop = () => {
         }
         setCart(savedCart);
     }, [products])
+
+    useEffect( () =>{
+        fetch(`http://localhost:5000/product?page=${selectpage}&size=${size} `)
+        .then(res => res.json())
+        .then(data => setProducts(data));
+
+    }, [selectpage,size]);
+
+
+
+    //This useEffect use for pagination
+    useEffect(()=>{
+        fetch('http://localhost:5000/productCount')
+        .then (res => res.json())
+        .then (data => {
+            const count = data.count;
+            const pag = Math.ceil(count/10);
+            setPages(pag);
+        })
+    },[])
 
     const handleAddToCart = (selectedProduct) =>{
         console.log(selectedProduct);
@@ -52,6 +78,21 @@ const Shop = () => {
                         handleAddToCart={handleAddToCart}
                         ></Product>)
                 }
+                    {/*Important Dynamic CSS Impimatation in react  */}
+                <div className='pagination'>{
+                    [...Array(pages).keys()].map(number => 
+                    <button onClick={()=>setSelectPage(number)} className={selectpage === number ? 'selected': ''}> 
+                        {number}
+                    </button>)
+                }
+                <select name ="" onChange={e=> setSize(e.target.value)}>
+                    <option value='5'>5</option>
+                    <option value='10'>10</option>
+                    <option value='15'>15</option>
+                    <option value='20'>20</option>
+                </select>
+                </div>
+
             </div>
             <div className="cart-container">
                 <Cart cart={cart}>
